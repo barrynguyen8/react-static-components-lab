2baeae10b8973849d2b1defb3bac0d8d70546e00
Barry Nguyen <barry.tuan.nguyen@gmail.com>
2021-03-30 23:39:09 +1100

    assigment submitted

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import CatComponent from './CatComponent'; 
+
 ReactDOM.render(<App />, document.getElementById('root'));
